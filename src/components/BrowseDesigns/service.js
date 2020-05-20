983102c808b5bcf4bf7d1c53ee934f8a3a444394
Amanda Cox <amandamcox@gmail.com>
2020-05-20 16:04:47 -0700

    Added filter by one category on browse page

--- src/components/BrowseDesigns/service.js
+++ src/components/BrowseDesigns/service.js
@@ -9,6 +9,15 @@ export const getAllDesigns = async () => {
 	}
 }
 
+export const getFilteredDesigns = async category => {
+	try {
+		const res = await axios.get(`/api/designs?category=${category}`)
+		return res.data
+	} catch (error) {
+		throw Error(error)
+	}
+}
+
 export const rateDesign = async (id, vote) => {
 	try {
 		const res = await axios.post(`/api/designs/${id}/${vote}`)
